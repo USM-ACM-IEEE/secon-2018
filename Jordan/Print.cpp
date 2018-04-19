e12708094650d4fffaa9ec3db56b12285449d5b3
Jordan Parker <31749489+jordanparker32@users.noreply.github.com>
2018-04-18 18:09:10 -0600

    Update Print.cpp

--- Jordan/Print.cpp
+++ Jordan/Print.cpp
@@ -1,19 +1,11 @@
 #include <LiquidCrystal.h>
 
-int pin1;
-int pin2;
-int pin3;
-int pin4;
+int pin1, pin2, pin3, pin4;
 int rem, temp, dec = 0, b = 1;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-
-void binDec(int num);
-
-
 void setup() {
   lcd.begin(16, 2);                  // set up the LCD's number of columns and rows
-  lcd.print("#");                    // print
 
   // setting pins 6-9 to input
   pinMode(6, INPUT);
@@ -24,34 +16,25 @@ void setup() {
 }
 
 void loop() {
- 
-  lcd.setCursor(0, 1);               // set the cursor to column 0, line 1 because count starts at 0
 
   pin1 = digitalRead(6);
   pin2 = digitalRead(7);
   pin3 = digitalRead(8);
   pin4 = digitalRead(9);
 
-  binDec(pin1);
-  binDec(pin2);
-  binDec(pin3);
-  binDec(pin4);
-
-}
-
-//==================== Binary to Decimal ====================
-void binDec(int num){
-  temp = num;
-  while (num > 0){
-     rem = temp % 10;
-     dec = dec + rem * b;
-     b *= 2;
-     temp /= 10;
-  }
-  if( temp == 7){
+  int val = 4 * pin1 + 2 * pin2 + 1 * pin3;
+  
+  
+  if(!pin4 && val == 7){
+    lcd.setCursor(0, 1); 
     lcd.print("Aligned");
   }
   else {
-    lcd.print(temp);
+    lcd.setCursor(0,0);
+    lcd.print(val);
+    lcd.setCursor(0,1);
+    lcd.print("                          ");
   }
+
 }
+
